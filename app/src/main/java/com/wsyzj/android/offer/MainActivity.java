dacf5747aa08e8236576395dc71f31cc69f1650a
jiao35478729@163.com <j13721406352>
2017-02-18 10:21:41 +0800

    第一次提交代码

--- app/src/main/java/com/wsyzj/android/offer/MainActivity.java
+++ app/src/main/java/com/wsyzj/android/offer/MainActivity.java
@@ -3,6 +3,9 @@ package com.wsyzj.android.offer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 为了Offer
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
