40cd6e95638ee13cad63852a74f9cda37697b99d
jiao35478729@163.com <j13721406352>
2017-02-19 22:47:27 +0800

    添加了图文混排

--- app/src/main/java/com/wsyzj/android/offer/MainActivity.java
+++ app/src/main/java/com/wsyzj/android/offer/MainActivity.java
@@ -1,16 +1,46 @@
 package com.wsyzj.android.offer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.wsyzj.android.offer.activity.ImageTextSortAct;
+
+import java.util.Arrays;
 
 /**
- * 为了Offer
+ * Offer
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private ListView list_view;
+    private String[] mDatas = new String[]{"图文混排"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list_view = (ListView) findViewById(R.id.list_view);
+        ArrayAdapter arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Arrays.asList(mDatas));
+        list_view.setAdapter(arrayAdapter);
+        list_view.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case 0:
+                startAct(ImageTextSortAct.class);
+                break;
+        }
+    }
+
+    private void startAct(Class clas) {
+        startActivity(new Intent(this, clas));
     }
 }
---
+++ app/src/main/java/com/wsyzj/android/offer/tools/LogUtils.java
@@ -0,0 +1,201 @@
+package com.wsyzj.android.offer.tools;
+
+
+public class LogUtils {
+    /**
+     * 日志输出级别NONE
+     */
+
+    public static final int LEVEL_NONE = 0;
+
+    /**
+     * 日志输出级别V
+     */
+
+    public static final int LEVEL_VERBOSE = 1;
+
+    /**
+     * 日志输出级别D
+     */
+
+    public static final int LEVEL_DEBUG = 2;
+
+    /**
+     * 日志输出级别I
+     */
+
+    public static final int LEVEL_INFO = 3;
+
+    /**
+     * 日志输出级别W
+     */
+
+    public static final int LEVEL_WARN = 4;
+
+    /**
+     * 日志输出级别E
+     */
+
+    public static final int LEVEL_ERROR = 5;
+
+    /**
+     * 日志输出时的TAG
+     */
+
+    private static String mTag = "哈哈";
+
+    /**
+     * 是否允许输出log
+     */
+
+    private static int mDebuggable = LEVEL_ERROR;
+
+    /**
+     * 用于记时的变量
+     */
+
+    private static long mTimestamp = 0;
+
+    /**
+     * 以级别为 d 的形式输出LOG
+     */
+
+    public static void v(String msg) {
+
+        if (mDebuggable >= LEVEL_VERBOSE) {
+
+            android.util.Log.v(mTag, msg);
+
+        }
+
+    }
+
+    /**
+     * 以级别为 d 的形式输出LOG
+     */
+
+    public static void d(String msg) {
+
+        if (mDebuggable >= LEVEL_DEBUG) {
+
+            android.util.Log.d(mTag, msg);
+
+        }
+
+    }
+
+    /**
+     * 以级别为 i 的形式输出LOG
+     */
+
+    public static void i(String msg) {
+
+        if (mDebuggable >= LEVEL_INFO) {
+
+            android.util.Log.i(mTag, msg);
+
+        }
+
+    }
+
+    /**
+     * 以级别为 w 的形式输出LOG
+     */
+
+    public static void w(String msg) {
+
+        if (mDebuggable >= LEVEL_WARN) {
+
+            android.util.Log.w(mTag, msg);
+
+        }
+
+    }
+
+    /**
+     * 以级别为 w 的形式输出Throwable
+     */
+
+    public static void w(Throwable tr) {
+
+        if (mDebuggable >= LEVEL_WARN) {
+
+            android.util.Log.w(mTag, "", tr);
+
+        }
+
+    }
+
+    /**
+     * 以级别为 w 的形式输出LOG信息和Throwable
+     */
+
+    public static void w(String msg, Throwable tr) {
+
+        if (mDebuggable >= LEVEL_WARN && null != msg) {
+
+            android.util.Log.w(mTag, msg, tr);
+
+        }
+
+    }
+
+    /**
+     * 以级别为 e 的形式输出LOG
+     */
+
+    public static void e(String msg) {
+
+        if (mDebuggable >= LEVEL_ERROR) {
+
+            android.util.Log.e(mTag, msg);
+
+        }
+
+    }
+
+    /**
+     * 以级别为 e 的形式输出Throwable
+     */
+
+    public static void e(Throwable tr) {
+
+        if (mDebuggable >= LEVEL_ERROR) {
+
+            android.util.Log.e(mTag, "", tr);
+
+        }
+
+    }
+
+    /**
+     * 以级别为 e 的形式输出LOG信息和Throwable
+     */
+
+    public static void e(String msg, Throwable tr) {
+
+        if (mDebuggable >= LEVEL_ERROR && null != msg) {
+
+            android.util.Log.e(mTag, msg, tr);
+
+        }
+
+    }
+
+    /**
+     * 以级别为 e 的形式输出msg信息,附带时间戳，用于输出一个时间段结束点* @param msg 需要输出的msg
+     */
+
+    public static void elapsed(String msg) {
+
+        long currentTime = System.currentTimeMillis();
+
+        long elapsedTime = currentTime - mTimestamp;
+
+        mTimestamp = currentTime;
+
+        e("[Elapsed：" + elapsedTime + "]" + msg);
+
+    }
+
+}
