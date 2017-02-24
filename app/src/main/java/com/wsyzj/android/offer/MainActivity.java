d2d2219d1d10a46f1ddeb0fb5c370061cd934e97
jiao35478729@163.com <j13721406352>
2017-02-24 23:05:56 +0800

    添加了部分说明

--- app/src/main/java/com/wsyzj/android/offer/MainActivity.java
+++ app/src/main/java/com/wsyzj/android/offer/MainActivity.java
@@ -13,7 +13,21 @@ import com.wsyzj.android.offer.activity.LottieAct;
 import java.util.Arrays;
 
 /**
- * Offer
+ * 1: 快捷键
+ * -- 想要预览TextView有值时的效果 添加自定义命名空间:xmlns:tools="http://schemas.android.com/tools"
+ * --- tools:text="输入模拟的文本"(运行时不会出现)
+ * -- 快捷生成布局文件中的@string @dimen     alt+enter → Extract String/Dimension Resource
+ * -- 关闭菜单栏中的其他文件，保存当前文件    alt(鼠标点击当前文件的关闭标志)
+ * -- 查看当前类继承关系和所有的复写方法      ctrl+o
+ * -- 当前文件以单独的一个窗口打开           shift+f4
+ * -- 自动生成匹配正则表达式                 alt+enter → check regexp
+ * -- 快速切换文件                          ctrl+tab
+ * 2:  SVN
+ * -- 忽略文件设置 File → Setting → Ignored Files
+ * --- Directory:.gradle/ && Directory:.idea/ && Directory:build/ && Directory: app/build/ && File:local.properties && Mask:*.iml
+ * -- 解除项目与SVN关联
+ * --- 1: .idea文件夹找到vcs.xml → vcs="svn"改为vcs=""保存
+ * --- 2: 把项目根目录.svn文件夹删除
  */
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
