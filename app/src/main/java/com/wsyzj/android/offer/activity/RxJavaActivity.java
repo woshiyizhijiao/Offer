2621a2ee800e2afc5dfec4b341009837cd2ba63a
benjiao <benjiao@pacewear.cn>
2017-07-30 17:50:40 +0800

    继续学习rxjava，每天学习一点也是学啊，总不能吃了睡，睡了吃吧。

--- app/src/main/java/com/wsyzj/android/offer/activity/RxJavaActivity.java
+++ app/src/main/java/com/wsyzj/android/offer/activity/RxJavaActivity.java
@@ -11,10 +11,17 @@ import com.wsyzj.android.offer.mvp.presenter.RxJavaPresenter;
 import com.wsyzj.android.offer.tools.LogUtils;
 import com.wsyzj.android.offer.widget.BaseDialog;
 
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableEmitter;
+import io.reactivex.FlowableOnSubscribe;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
@@ -22,8 +29,10 @@ import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
 import static io.reactivex.Observable.create;
@@ -247,5 +256,231 @@ public class RxJavaActivity extends BaseActivity implements RxJavaContract.View 
                 LogUtils.e(s);
             }
         });
+    }
+
+    /**
+     * zip操作符
+     * 将上游的两个管道按顺序合并，合并的最终长度为管道事件最少的的管道。默认是在同一个线程中完成，想要同时进行，需要设置Schedulers.io()
+     */
+    public void zip1(View v) {
+        Observable observable1 = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(1);
+                LogUtils.e("onNext 1");
+                emitter.onNext(2);
+                LogUtils.e("onNext 2");
+                emitter.onNext(3);
+                LogUtils.e("onNext 3");
+                emitter.onNext(4);
+                LogUtils.e("onNext 4");
+                emitter.onComplete();
+                LogUtils.e("onComplete 1");
+            }
+        }).subscribeOn(Schedulers.io());
+
+        Observable observable2 = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                emitter.onNext("A");
+                LogUtils.e("onNext A");
+                emitter.onNext("B");
+                LogUtils.e("onNext B");
+                emitter.onNext("C");
+                LogUtils.e("onNext C");
+                emitter.onComplete();
+                LogUtils.e("onComplete 2");
+            }
+        }).subscribeOn(Schedulers.io());
+
+        Observable.zip(observable1, observable2, new BiFunction<Integer, String, String>() {
+
+            @Override
+            public String apply(Integer integer, String s) throws Exception {
+                return integer + s;
+            }
+        }).subscribe(new Observer() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                LogUtils.e("onSubscribe");
+            }
+
+            @Override
+            public void onNext(Object value) {
+                LogUtils.e("onNext : " + value);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                LogUtils.e("onError : ");
+            }
+
+            @Override
+            public void onComplete() {
+                LogUtils.e("onComplete : ");
+            }
+        });
+    }
+
+    /**
+     * 上游两个管道的其中一个在无线发送事件，而另一个管道只发送一个，最终导致oom
+     */
+    public void zip2(View v) {
+        Observable observable1 = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                for (int x = 0; ; x++) {
+                    emitter.onNext(1);
+                }
+            }
+        }).subscribeOn(Schedulers.io());
+
+        Observable<String> observable2 = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                emitter.onNext("第二个管道");
+            }
+        }).subscribeOn(Schedulers.io());
+
+        Observable.zip(observable1, observable2, new BiFunction<Integer, String, String>() {
+            /**
+             * Calculate a value based on the input values.
+             *
+             * @param integer the first value
+             * @param s       the second value
+             * @return the result value
+             * @throws Exception on error
+             */
+            @Override
+            public String apply(Integer integer, String s) throws Exception {
+                return integer + s;
+            }
+
+        }).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Object value) {
+                        LogUtils.e("onNext" + value);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+    }
+
+    /**
+     * filter
+     *
+     * @param v
+     */
+    public void filter(View v) {
+        Observable
+                .create(new ObservableOnSubscribe<Integer>() {
+                    @Override
+                    public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                        for (int i = 0; ; i++) {
+                            emitter.onNext(i);
+                        }
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .filter(new Predicate<Integer>() {
+                    @Override
+                    public boolean test(Integer integer) throws Exception {
+                        return integer % 10 == 0;
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        LogUtils.e("accept" + integer);
+                    }
+                });
+    }
+
+    /**
+     * sample代表每隔指定时间从上游事件中发送消息到下游
+     *
+     * @param v
+     */
+    public void sample(View v) {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                for (int i = 0; ; i++) {
+                    emitter.onNext(i);
+                }
+            }
+        }).subscribeOn(Schedulers.io())
+                .sample(2, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        LogUtils.e("accept : " + integer);
+                    }
+                });
+    }
+
+    /**
+     * flowable用法
+     *
+     * @param v
+     */
+    public void flowable(View v) {
+        Flowable<Integer> flowable = Flowable.create(new FlowableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(FlowableEmitter<Integer> e) throws Exception {
+                e.onNext(1);
+                LogUtils.e("Flowable : 1");
+                e.onNext(2);
+                LogUtils.e("Flowable : 2");
+                e.onNext(3);
+                LogUtils.e("Flowable : 3");
+                e.onComplete();
+            }
+        }, BackpressureStrategy.ERROR);
+
+//        Drop就是直接把存不下的事件丢弃,Latest就是只保留最新的事件
+
+        Subscriber subscriber = new Subscriber<Integer>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                LogUtils.e("onSubscribe");
+                s.request(Long.MAX_VALUE);  // 不加这行代码会报错 MissingBackpressureException
+            }
+
+            @Override
+            public void onNext(Integer object) {
+                LogUtils.e("onNext : " + object);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                LogUtils.e("onError");
+                t.printStackTrace();
+            }
+
+            @Override
+            public void onComplete() {
+                LogUtils.e("onComplete");
+            }
+        };
+
+        flowable.subscribe(subscriber);
     }
 }
--- app/src/main/java/com/wsyzj/android/offer/offer/OfferJava.java
+++ app/src/main/java/com/wsyzj/android/offer/offer/OfferJava.java
@@ -1,16 +1,5 @@
 package com.wsyzj.android.offer.offer;
 
-import android.text.TextUtils;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 /**
  * @author: wsyzj
  * @date: 2017-03-01 22:35
@@ -19,64 +8,6 @@ import java.util.Set;
 public class OfferJava {
 
     public static void main(String[] array) {
-        List<String> list = new ArrayList<>();
-        System.out.println(removeDuplicate(list));
-    }
-
-    /**
-     * 对该集合的值按小到大的顺序进行排序，key=value不变，
-     *
-     * @param ages
-     * @return
-     */
-    private static LinkedHashMap<Integer, Integer> sortHashMap(Map<Integer, Integer> ages) {
-        Set<Map.Entry<Integer, Integer>> map = ages.entrySet();
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map);
-        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
-            @Override
-            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
-                return o1.getValue() - o2.getValue();
-            }
-        });
-
-        LinkedHashMap<Integer, Integer> linkedHashMap = new LinkedHashMap<>();
-        for (Map.Entry<Integer, Integer> entry : list) {
-            linkedHashMap.put(entry.getKey(), entry.getValue());
-        }
-        return linkedHashMap;
-    }
-
-    /**
-     * 去除集合中的重复元素
-     * 方法二  ，遍历集合中的元素，创建一个新的集合，如在集合中没有该元素则添加到新的集合中
-     *
-     * @param list
-     * @return
-     */
-    private static List<String> removeDuplicate(List list) {
-        HashSet hashSet = new HashSet(list);
-        list.clear();
-        list.addAll(hashSet);
-        return list;
-    }
-
-    /**
-     * byte转int
-     *
-     * @param b
-     * @return
-     */
-    public static int byteToInt(byte b) {
-        String s = b + "";
-        if (!TextUtils.isEmpty(s)) {
-            try {
-                return Integer.parseInt(s);
-            } catch (Exception e) {
-                e.printStackTrace();
-                return -1;
-            }
-        } else {
-            return -1;
-        }
+        System.out.println();
     }
 }
